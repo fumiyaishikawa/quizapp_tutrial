380e152ea79730790c5d072aeebed331ad617e96
fumiya ishikawa <programer.ishikawa@gmail.com>
2020-03-08 11:45:47 +0900

    クイズAPIをVue側で表示

--- laravel_vue_quiz/resources/js/components/page/Quiz.vue
+++ laravel_vue_quiz/resources/js/components/page/Quiz.vue
@@ -126,6 +126,30 @@ export default {
         });
     },
     methods: {
+        goAnswer(selectAnswerNum) {
+            if (selectAnswerNum === 0) {
+                // selectAnswerNumが0の場合は、click 「正解を表示する」ボタンのクリック alert-info、alert-dangerを非表示
+                this.isCorrect = false;
+                this.isMistake = false;
+            } else if (selectAnswerNum === Number(this.correctAnswerNo)) {
+                // 正解を押した場合 alert-infoを表示し、alert-dangerを非表示にする そしてスコアを加算する
+                this.isCorrect = true;
+                this.isMistake = false;
+                this.score += 1;
+            } else {
+                // 不正解の場合 alert-infoを非表示し、alert-dangerを表示にする
+                this.isMistake = true;
+                this.isCorrect = false;
+            }
+            // 回答済みの設定をONにする 同じ問題に２回以上の回答をさせないため、そして解説を表示するため
+            this.isAlreadyAnswered = true;
+
+            // 10問以上回答している場合は、クイズを終了
+            if (this.quizNumber >= 10) {
+                this.endQuiz();
+            }
+        },
+        // クイズの表示
         findNextQuiz(quizNumber) {
             this.title = this.quizData[quizNumber].title;
             this.answers = [
@@ -139,6 +163,25 @@ export default {
                 quizNumber
             ].answer.correct_answer_no;
             this.categoryName = this.quizData[quizNumber].category.name;
+        },
+        goNextQuiz() {
+            // 次の問題へをクリック
+            if (this.quizNumber >= 10) {
+                // １０問以上の場合はクイズを終了
+                this.endQUiz();
+            } else {
+                // 次のクイズを表示し、クイズ番号を加算する。laert-info, alert-danger、解説を非表示にする
+                this.findNextQuiz(this.quizNumber);
+                this.quizNumber += 1;
+                this.isCOrrect = false;
+                this.isMistake = false;
+                this.isAlreadyAnswered = false;
+            }
+        },
+        endQUiz() {
+            this.isQuizFinish = true;
+            this.answerNo = "-";
+            this.isAlreadyAnswered = true;
         }
     }
 };
